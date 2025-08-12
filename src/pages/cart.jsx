5584c65495b490f461ea229bc7a4187d4d7049de
Asad <hellohello51981@gmail.com>
2025-08-12 16:22:22 +0500

    Pic Some: total price is Added in US currency

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -4,13 +4,18 @@ import CartItem from "../components/cartItem"
 const Cart = () => {
   const {cartItem} = useContext(Context)
 
+  const total = 6.8 * cartItem.length
+   const totalPrice = total.toLocaleString("en-US", {style: "currency", currency: "USD"})
+
   const cartElement = cartItem.map(item => (
     <CartItem key={item.id} item={item} />
   ))
   console.log(cartItem)
   return (
     <div>
+      <h1 className="text-3xl font-bold text-center m-4">Cart Items</h1>
       {cartElement}
+      <h2 className="text-xl font-semibold text-right m-4">Total Price: {totalPrice}</h2>
     </div>
   )
 }
