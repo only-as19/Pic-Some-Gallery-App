b3080a1059ce754a0d53dc2ac5bdfd0c49b56304
Asad <hellohello51981@gmail.com>
2025-08-12 16:15:38 +0500

    Pic Some: style the cartItem to show image and price

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,8 +1,13 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { Context } from '../context/context'
+import { useContext } from 'react'
 const CartItem = ({item}) => {
   return (
-    <div>
-      <p>{item.id}</p>
+    <div className='text-[20px] flex justify-between items-center m-5'>
+        <FontAwesomeIcon icon={faTrash} className='hover:text-blue-500' onClick={()=> removeItem(item.id)}/>
+      <img className='w-[170px] h-30 mr-auto ml-3 rounded shadow-lg object-cover' src={item.url} alt="" />
+      <p className='font-bold'>$6.8</p>
     </div>
   )
 }
