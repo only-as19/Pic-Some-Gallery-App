85255c8c24be2b0bd8dfc7ee77dc5c3f43827e82
Asad <hellohello51981@gmail.com>
2025-08-12 16:16:46 +0500

    Pic Some: Remove item From cart is noe working

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -2,10 +2,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 import { Context } from '../context/context'
 import { useContext } from 'react'
+
 const CartItem = ({item}) => {
+    const {removeFromCart} =useContext(Context)
   return (
     <div className='text-[20px] flex justify-between items-center m-5'>
-        <FontAwesomeIcon icon={faTrash} className='hover:text-blue-500' onClick={()=> removeItem(item.id)}/>
+        <FontAwesomeIcon icon={faTrash} className='hover:text-blue-500' onClick={()=> removeFromCart(item.id)}/>
       <img className='w-[170px] h-30 mr-auto ml-3 rounded shadow-lg object-cover' src={item.url} alt="" />
       <p className='font-bold'>$6.8</p>
     </div>
