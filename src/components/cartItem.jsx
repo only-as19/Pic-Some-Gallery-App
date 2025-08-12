d4ba554d688abbd8be4ff31089820608533ecd31
Asad <hellohello51981@gmail.com>
2025-08-12 16:13:52 +0500

    Pic Some: add to cart is working & add new component

---
+++ src/components/cartItem.jsx
@@ -0,0 +1,10 @@
+
+const CartItem = ({item}) => {
+  return (
+    <div>
+      <p>{item.id}</p>
+    </div>
+  )
+}
+
+export default CartItem
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,8 +1,16 @@
-
+import { useContext } from "react"
+import { Context } from "../context/context"
+import CartItem from "../components/cartItem"
 const Cart = () => {
+  const {cartItem} = useContext(Context)
+
+  const cartElement = cartItem.map(item => (
+    <CartItem key={item.id} item={item} />
+  ))
+  console.log(cartItem)
   return (
     <div>
-      Cart
+      {cartElement}
     </div>
   )
 }
